311a97aed4679c8ccb7529ff0e515b2c3b0230d6
joaov1ct0r <jov9051@outlook.com>
2023-04-02 19:36:56 -0300

    refactor: BubbleSortAlg

---
+++ src/com/bubbleSortAlg/BubbleSort.java
@@ -0,0 +1,18 @@
+package com.bubbleSortAlg;
+
+public class BubbleSort {
+    public int[] execute(int[] array) {
+        int arrLength = array.length;
+        int temp;
+        for (int i = 0; i < arrLength; i++) {
+            for (int j = 0; j < (arrLength - i - 1); j++) {
+                if (array[j] > array[j + 1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        return array;
+    }
+}
---
+++ src/com/bubbleSortAlg/Main.java
@@ -0,0 +1,15 @@
+package com.bubbleSortAlg;
+
+public class Main {
+    public static void main(String[] args) {
+        BubbleSort bubbleSort = new BubbleSort();
+
+        int[] array = new int[] {78, 45, 23, 67, 65, 11, 90, 100, 1493};
+
+        int[] result = bubbleSort.execute(array);
+
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
+}
